070bc42c5cb8093d5280aae51bdb3a53192172c4
storngfornt <au0038173@gmail.com>
2024-09-06 11:37:05 +0600

    implementing pagination and category filter system

---
+++ src/pages/ProductPage/Pagination.jsx
@@ -0,0 +1,113 @@
+/* eslint-disable react/prop-types */
+
+export default function Pagination({
+  setCurrentPage,
+  currentPage,
+  totalPages,
+}) {
+  const paginationCount = [...Array(totalPages).keys()].map((c) => c + 1);
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNext = () => {
+    if (currentPage < paginationCount?.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  return (
+    <>
+      {/* Pagination Controls */}
+      {/* <div className="pagination">
+      <button
+        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+
+      {[...Array(totalPages).keys()].map(number => (
+        <button
+          key={number + 1}
+          onClick={() => setCurrentPage(number + 1)}
+          className={currentPage === number + 1 ? "active" : ""}
+        >
+          {number + 1}
+        </button>
+      ))}
+
+      <button
+        onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div> */}
+      <div className="mb-6 mt-6 flex items-center  justify-center">
+        <nav
+          aria-label="Pagination"
+          className="inline-flex -space-x-px bg-gray-50  rounded-md shadow-sm"
+        >
+          <button
+            onClick={handlePrev}
+            type="button"
+            className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-l-md dark:border-gray-300"
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              className="w-5 h-5 text-black"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          {paginationCount?.map((c, i) => (
+            <button
+              key={i}
+              type="button"
+              aria-current="page"
+              className={`inline-flex items-center px-4 py-2  text-sm font-semibold border ${
+                currentPage === c ? "bg-[#1E99F5] text-white" : "text-black"
+              } 
+           dark:border-gray-300`}
+              onClick={() => setCurrentPage(c)}
+            >
+              {c}
+            </button>
+          ))}
+
+          <button
+            onClick={handleNext}
+            type="button"
+            className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-r-md dark:border-gray-300"
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              className="w-5 h-5 text-black "
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </nav>
+      </div>
+    </>
+  );
+}
--- src/pages/ProductPage/Products.jsx
+++ src/pages/ProductPage/Products.jsx
@@ -1,20 +1,36 @@
 import { Helmet } from "react-helmet-async";
 import ProductsCard from "./ProductsCard";
 import useContextProvider from "../../useHooks/useContextProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Pagination from "./Pagination";
 
 export default function Products() {
+  const { getProducts, productsData } = useContextProvider();
+  const [category, setCategory] = useState("Rocking Chair");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
- const {getProducts, productsData} = useContextProvider();
-
- useEffect(()=> {
+  useEffect(() => {
     getProducts();
+  }, []);
 
- }, []);
+  //filtered products by category ==========
+  const filteredProducts = productsData.filter(
+    (product) => product.category === category
+  );
 
- console.log(productsData);
- 
+  // Calculate pagination indices for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
+  // Get the current products for the page
+  const currentProducts = filteredProducts.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
 
   return (
     <>
@@ -26,28 +42,57 @@ export default function Products() {
         *
       </div>
 
-      <main className=" my-16 px-2 md:px-4 lg:px-8 flex gap-4 md:gap-6  lg:gap-10  xl:gap-16 ">
+      <main className=" mt-16 px-2 md:px-4 lg:px-8 flex gap-4 md:gap-6  lg:gap-10  xl:gap-16 ">
         {/* categories side ================= */}
         <aside className="flex flex-col min-w-fit gap-2 border border-[#E8E8E8] p-3  pl-1 rounded-sm ">
-          <button className="px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl  font-semibold border rounded-md py-1 text-[#717171] hover:text-white hover:bg-black duration-300  ">
+          <button
+            onClick={() => setCategory("Rocking Chair")}
+            className={`${
+              category === "Rocking Chair"
+                ? "bg-black text-white"
+                : "text-[#717171]"
+            } px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl  font-semibold border rounded-md py-1  hover:text-white hover:bg-gray-800 duration-300 `}
+          >
             Rocking chair
           </button>
-          <button className="px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl  font-semibold border rounded-md py-1 text-[#717171] hover:text-white hover:bg-black duration-300  ">
+          <button
+            onClick={() => setCategory("Side Chair")}
+            className={`${
+              category === "Side Chair"
+                ? "bg-black text-white"
+                : "text-[#717171]"
+            } px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl  font-semibold border rounded-md py-1 hover:text-white hover:bg-gray-800 duration-300`}
+          >
             Side chair
           </button>
-          <button className="px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl   font-semibold border rounded-md py-1 text-[#717171] hover:text-white hover:bg-black duration-300  ">
+          <button
+            onClick={() => setCategory("Lounge Chair")}
+            className={`${
+              category === "Lounge Chair"
+                ? "bg-black text-white"
+                : "text-[#717171]"
+            } px-2 md:px-4 text-start text-sm md:text-lg xl:text-xl  font-semibold border rounded-md py-1  hover:text-white hover:bg-gray-800 duration-300`}
+          >
             Lounge chair
           </button>
         </aside>
         {/* categories side ================= */}
         {/* product side ============ */}
-        <div className=" grid md:grid-cols-2  lg:grid-cols-3 gap-4">
-            {
-                productsData?.map((product, idx) => (
-                    <ProductsCard  key={idx} product={product}  />
-                ) )
-            }
-         
+        <div>
+          <div className=" grid md:grid-cols-2  lg:grid-cols-3 gap-4">
+            {currentProducts?.map((product, idx) => (
+              <ProductsCard key={idx} product={product} />
+            ))}
+          </div>
+          {/* 
+          pagination =============== */}
+          <div className="mb-10 mt-5">
+            <Pagination
+              setCurrentPage={setCurrentPage}
+              totalPages={totalPages}
+              currentPage={currentPage}
+            />
+          </div>
         </div>
         {/* product side ============ */}
       </main>
